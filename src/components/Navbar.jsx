156ccdaa9a3389b021287b19faea9d3c04065212
souvikrajsingh <souvikrajsingh@gmail.com>
2024-02-25 22:06:56 +0530

    fixed-signin-options

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,13 +16,29 @@ function Navbar() {
                             <li><Link to='/Explore'>Explore</Link></li>
                             <li><Link to='/Treks'>Treks</Link></li>
                             <li><Link to='/BeAGuide'>Be a Guide</Link></li>
-                            <li><Link to='/Profile'>Profile</Link></li>
                         </ul>
                     </div>
                     {/* <div className='flex '>
             <FaSearch /><input className="pl-1" placeholder="Type to search" />
           </div> */}
                     <div>
+                    <Link to="/Profile/Signin">
+                <button
+                    type="button"
+                    className="rounded-full bg-black px-3 py-2 mb-4 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                >
+                    Sign in
+                </button>
+            </Link>
+
+            <Link to="/Profile/Signup" className='px-2'>
+                <button
+                    type="button"
+                    className="rounded-full bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                >
+                    Sign up
+                </button>
+            </Link>
                         <button className='h-10'><MdDarkMode /></button>
                     </div>
                 </nav>
