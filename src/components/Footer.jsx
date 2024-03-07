0617aa3e042a5ab9a6c41d35eedb180e2e8b2c1d
Panchammmm <sardarpancham510510@gmail.com>
2024-03-07 19:11:29 +0530

    copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,56 +1,66 @@
 import React from "react";
 import { ChevronRight } from 'lucide-react'
 
-// function Footer() {
-//     const year = new Date().getFullYear();
-//     return (
-//         <>
-//             {footerOne()},
-//             <footer>
-//                 <p>Copyright ⓒ {year}</p>
-//             </footer>
-//         </>
-//     );
-// }
+function Copyright() {
+    const year = new Date().getFullYear();
+    return (
+        <>
+            <footer>
+                <p>Copyright ⓒ {year}</p>
+            </footer>
+        </>
+    );
+}
 
 function Footer() {
     return (
-        <footer className="w-full">
-            
-            <hr className="my-8" />
-            <div className="mx-auto flex max-w-6xl flex-col items-start space-x-8 md:flex-row">
-                <div className="w-full px-4 md:w-1/2 lg:px-0">
-                    <h1 className="max-w-sm text-3xl font-bold">Subscribe to TravelHike</h1>
-                    <form action="" className="mt-4 inline-flex w-full items-center md:w-3/4">
-                        <input
-                            className="flex h-10 w-full rounded-md border border-black/20 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                            type="email"
-                            placeholder="Email"
-                        ></input>
-                        <button
-                            type="button"
-                            className="ml-4 rounded-full bg-black px-3 py-3 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-                        >
-                            <ChevronRight className="h-4 w-4" />
-                        </button>
-                    </form>
+        <>
+            <footer className="w-full">
+
+                <hr className="my-8" />
+                <div className="mx-auto flex max-w-6xl flex-col items-start space-x-8 md:flex-row">
+                    <div className="w-full px-4 md:w-1/2 lg:px-0">
+                        <h1 className="max-w-sm text-3xl font-bold">Subscribe to TravelHikes</h1>
+                        <form action="" className="mt-4 inline-flex w-full items-center md:w-3/4">
+                            <input
+                                className="flex h-10 w-full rounded-md border border-black/20 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                                type="email"
+                                placeholder="Email"
+                            ></input>
+                            <button
+                                type="button"
+                                className="ml-4 rounded-full bg-black px-3 py-3 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                            >
+                                <ChevronRight className="h-4 w-4" />
+                            </button>
+                        </form>
+                    </div>
+                    <div className="mt-8 grid grid-cols-2 gap-6 md:mt-0 lg:w-3/4 lg:grid-cols-3">
+                        {Array.from({ length: 3 }).map((_, i) => (
+                            <div key={i} className="mb-8 lg:mb-0">
+                                <p className="mb-6 text-lg font-semibold text-gray-700 ">Company</p>
+                                <ul className="flex flex-col space-y-4 text-[14px] font-medium text-gray-500">
+                                    <li>About us</li>
+                                    <li>Company History</li>
+                                    <li>Our Team</li>
+                                    <li>Our Vision</li>
+                                    <li>Press Release</li>
+                                </ul>
+                            </div>
+                        ))}
+                    </div>
                 </div>
-                <div className="mt-8 grid grid-cols-2 gap-6 md:mt-0 lg:w-3/4 lg:grid-cols-3">
-                    {Array.from({ length: 3 }).map((_, i) => (
-                        <div key={i} className="mb-8 lg:mb-0">
-                            <p className="mb-6 text-lg font-semibold text-gray-700 ">Company</p>
-                            <ul className="flex flex-col space-y-4 text-[14px] font-medium text-gray-500">
-                                <li>About us</li>
-                                <li>Company History</li>
-                                <li>Our Team</li>
-                                <li>Our Vision</li>
-                                <li>Press Release</li>
-                            </ul>
-                        </div>
-                    ))}
+                <hr className="my-5" />
+
+            </footer>,
+
+            <div class="flex flex-col justify-end items-center">
+                <div class="mb-6 text-gray-400 font-medium">
+                    {Copyright()}
                 </div>
             </div>
-        </footer>
+
+        </>
     )
 }
 
