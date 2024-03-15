0534f41e645551d5014e8da4b9e333e0da371571
Panchammmm <sardarpancham510510@gmail.com>
2024-03-15 21:45:10 +0530

    explore-page

--- src/components/Explore-Page/Explore.jsx
+++ src/components/Explore-Page/Explore.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 import Navbar from "../Navbar.jsx";
 import Footer from "../Footer.jsx";
-import TopDestinaion from "./TopDestination.jsx";
-import Tabbar from "./Tabbar.jsx";
 import './explore.css';
 
 export default function Explore() {
   return (
     <>
       <Navbar />
-      <Tabbar />
-      <TopDestinaion />
       <Footer />
     </>
   );
